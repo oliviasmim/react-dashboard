9ffb01c24d9d35249ba104599ec10ecce3f2e62a
Kauã Kelvyn <ckaua504@gmail.com>
2024-11-17 14:55:44 -0300

    style (table): adding icon on button and colorful font

--- frontend/src/pages/dashboard/components/data-table/data-table.tsx
+++ frontend/src/pages/dashboard/components/data-table/data-table.tsx
@@ -7,32 +7,34 @@ import { FakeDataModel } from "../../../../services/fake-data-service";
 import { Button } from "react-bootstrap";
 import { colDefs, gridOptions } from "./constants";
 import { AddEntryModal } from "../add-entry-modal/add-entry-modal";
-import styles from "./data-table.module.css"
+import styles from "./data-table.module.css";
 
 export const DataTable = () => {
-	const data = useDataStore((state) => state.data);
-	// const isLoading = useDataStore((state) => state.isLoading);
-	const [showModal, setShowModal] = useState(false);
+  const data = useDataStore((state) => state.data);
+  // const isLoading = useDataStore((state) => state.isLoading);
+  const [showModal, setShowModal] = useState(false);
 
-	return (
-		<>
-			<div className={styles.tableHeader}>
-				<h5>Table data</h5>
-				<Button onClick={() => setShowModal(true)}>Add Entry</Button>
-			</div>
-			<div className="ag-theme-quartz" style={{ height: 500, width: "100%" }}>
-				<AgGridReact<FakeDataModel>
-					gridOptions={gridOptions}
-					rowData={data}
-					columnDefs={colDefs}
-					domLayout="autoHeight"
-					pagination={true}
-					paginationPageSize={15}
-					suppressServerSideFullWidthLoadingRow={true}
-					paginationPageSizeSelector={[15, 30, 50]}
-				/>
-			</div>
-			<AddEntryModal show={showModal} onHide={() => setShowModal(false)} />
-		</>
-	);
+  return (
+    <>
+      <div className={styles.tableHeader}>
+        <h5>Table data</h5>
+        <Button onClick={() => setShowModal(true)}>
+          <i className="bi bi-plus-lg"></i> Add Entry
+        </Button>
+      </div>
+      <div className="ag-theme-quartz" style={{ height: 500, width: "100%" }}>
+        <AgGridReact<FakeDataModel>
+          gridOptions={gridOptions}
+          rowData={data}
+          columnDefs={colDefs}
+          domLayout="autoHeight"
+          pagination={true}
+          paginationPageSize={15}
+          suppressServerSideFullWidthLoadingRow={true}
+          paginationPageSizeSelector={[15, 30, 50]}
+        />
+      </div>
+      <AddEntryModal show={showModal} onHide={() => setShowModal(false)} />
+    </>
+  );
 };
